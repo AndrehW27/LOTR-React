bcc5ba7c613840b2b3bef17c3a17ccd4a72f48a9
Andre Willian <andrehwillian@hotmail.com>
2021-03-12 16:16:38 -0300

    Quotes component modified by Ma

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -4,31 +4,45 @@ import LotrAPI from './LotrAPI';
 
 export default function Quotes() {
     const [quotesList, setQuotesList] = useState([]);
-    const [quoteIndex, setQuoteIndex] = useState([]);
-    const [listLength, setListLength] = useState([]);
-    const [randomNumber, setRandomNumber] = useState([]);
+    const [quoteIndex, setQuoteIndex] = useState(0);
+    const [listLength, setListLength] = useState(0);
+    const [randomNumber, setRandomNumber] = useState(0);
 
     useEffect(() => {
+
+
+        //  intercafe prosp=[
+        //      livros:{
+                 
+        //      },
+        //  ]
         const loadAllData = async () => {
 
  // GETTING QUOTES
  
             let listAllData = await LotrAPI.getAllData();   
-            console.log('list[2].items.docs.length: '+listAllData[2].items.docs.length);
-            setListLength(listAllData[2].items.docs.length)
-            console.log('listLength: '+listLength);
+            // console.log(listAllData[2].items.docs);
+            setQuotesList(listAllData[2].items.docs);
+            // console.log('teste'+quotesList);
+            
+            
+            // console.log('list[2].items.docs.length: '+listAllData[2].items.docs.length);
+            // console.log('list[2].items.docs.length: ->'+listAllData[2].items.docs.length+'<-');
+            // setListLength(listAllData[2].items.docs.length)
+            // console.log('listLength: '+listLength);
 
-            let quotesArray = []
-            for (let i = 0; i < listLength; i++) {
-                quotesArray.push(listAllData[2].items.docs[i].dialog);
-            }
-            setQuotesList(quotesArray);
-        console.log(quotesArray);  
-        console.log(quotesList);  
+            // let quotesArray = []
+            // for (let i = 0; i < listLength; i++) {
+            //     quotesArray.push(listAllData[2].items[i].dialog);
+            // }
+        //     setQuotesList(quotesArray);
+        // console.log("quotesArray: "+quotesArray);  
+        // console.log("quotesList: "+quotesList);  
+        
 
 
             setRandomNumber(Math.floor(Math.random() * listLength)); 
-            console.log('Random: '+randomNumber);
+            // console.log('Random: '+randomNumber);
             setQuoteIndex(randomNumber);
 
  // GETTING QUOTES
@@ -46,15 +60,31 @@ export default function Quotes() {
 
     function newQuote(){
         setQuoteIndex(Math.floor(Math.random() * listLength));      
-        console.log(quoteIndex);  
-        console.log(quotesList);  
+        // console.log("quoteIndex: "+quoteIndex);  
+        // console.log("quotesList: "+quotesList);  
 
     }
 
     return (
         <div className="containerQuotes">
             <h2>Momento sabedoria LOTR</h2>
-            <p>"{quotesList[quoteIndex]}"</p>
+            { console.log('teste')}
+
+
+        <div>
+        {quotesList.length >= 0 ?
+
+        <p>carregando</p>
+        
+        :           
+                    <p>"{quotesList[quoteIndex].dialog}"</p>
+        }
+        </div>
+            
+
+
+
+            
             <p>
                 By:
                 Movie:
